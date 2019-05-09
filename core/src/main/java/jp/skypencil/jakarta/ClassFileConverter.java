991c2a6aac3f6b22f6ad015de54ab2cbab70c190
Kengo TODA <skypencil@gmail.com>
2019-05-09 21:27:32 +0800

    refactor: extract Visitors

--- core/src/main/java/jp/skypencil/jakarta/ClassFileConverter.java
+++ core/src/main/java/jp/skypencil/jakarta/ClassFileConverter.java
@@ -4,11 +4,8 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 
 class ClassFileConverter implements ClassFileTransformer {
   @Override
@@ -27,61 +24,5 @@ class ClassFileConverter implements ClassFileTransformer {
     ClassWriter writer = new ClassWriter(reader, 0);
     reader.accept(new MyClassVisitor(Opcodes.ASM7, writer), 0);
     return writer.toByteArray();
-  }
-
-  static final class MyClassVisitor extends ClassVisitor {
-    public MyClassVisitor(int api, ClassVisitor another) {
-      super(api, another);
-    }
-
-    @Override
-    public MethodVisitor visitMethod(
-        final int access,
-        final String name,
-        final String descriptor,
-        final String signature,
-        final String[] exceptions) {
-      MethodVisitor mv = cv.visitMethod(access, name, descriptor, signature, exceptions);
-      return new MyMethodVisitor(api, mv);
-    }
-  }
-
-  static final class MyMethodVisitor extends MethodVisitor {
-    public MyMethodVisitor(int api, MethodVisitor mv) {
-      super(api, mv);
-    }
-
-    @Override
-    public void visitMethodInsn(
-        final int opcode,
-        String owner,
-        final String name,
-        final String descriptor,
-        final boolean isInterface) {
-      if (owner.startsWith("javax/")) {
-        owner = "jakarta/" + owner.substring(6);
-      }
-      mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
-    }
-
-    @Override
-    public void visitTypeInsn(final int opcode, String type) {
-      if (type.startsWith("javax/")) {
-        type = "jakarta/" + type.substring(6);
-      }
-      mv.visitTypeInsn(opcode, type);
-    }
-
-    @Override
-    public void visitLdcInsn(Object value) {
-      if (value instanceof Type) {
-        Type type = (Type) value;
-        String internalName = type.getInternalName();
-        if (internalName.startsWith("javax/")) {
-          value = Type.getObjectType("jakarta/" + type.getInternalName().substring(6));
-        }
-      }
-      mv.visitLdcInsn(value);
-    }
   }
 }
